1f58bb01ef5994fdea95add327777530d0d10b4c
KBrand <kbrandn25@gmail.com>
2025-11-20 18:23:26 +0100

    corrected linter errors

--- script.sh
+++ script.sh
@@ -2,23 +2,24 @@
 echo "=========================================="
 echo "   Guest a number to win between 0 to 5 "
 echo "=========================================="
-read -p "Enter the number of guest you want to play: " upper_limit
+echo "Enter the number of guest you want to play: "
+read -r upper_limit
 start_val=1;
  while [[ $start_val -le $upper_limit ]]
  do
   echo "Round $start_val"
   start_val=$((start_val+1))
  rand_num=$(( 0 + RANDOM % (5 - 0 + 1) ))
- 
- read -p "Enter the value you guest: " y
- if [[ $y -eq $rand_num ]] then
+   echo "Enter the value you guest: " 
+ read -r guess_num
+ if [[ $guess_num -eq $rand_num ]]; then
  echo "You WIN"
-  elif [[ $y -lt $rand_num ]] then
+  elif [[ $guess_num -lt $rand_num ]]; then
    echo "You Lose!"
    echo "you below the win number"
-   elif [[ $y -gt $rand_num ]] then
+   elif [[ $guess_num -gt $rand_num ]]; then
    echo "You Lose!"
-   #echo "you are above the win number"
+    echo "you above the win number"
  else
   echo "Invalid number, Not within the range"
  fi
